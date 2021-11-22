4330c6c0a0811be5755415aa77e4ab24a248e853
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-22 10:43:49 +0530

    counter useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import ComponentA from './components/contexthook/ComponentA';
+
+import CounterOne from './components/reducerhook/CounterOne';
 
 
 
-export const UserContext = React.createContext()
-export const BatchContext = React.createContext()
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'vamsi'} >
-        <BatchContext.Provider value={'dxc batch 2'}>
-          <ComponentA />
-        </BatchContext.Provider>
-      </UserContext.Provider>
+      <CounterOne/>
     </div>
   );
 }
